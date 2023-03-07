78e4bdae96e223596a4b894cc2a5e4986bb03e00
Tyler Kelly <tyler.kelly@outlook.com>
2023-03-07 20:59:02 +1000

    should be fixed

--- packup.sh
+++ packup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+VERSION=$(grep "version =" pyproject.toml | awk -F\" '{ print $2 }')
 dpkg-buildpackage -b -uc -us
 dpkg -b debian/aptupdatechecker
-mv debian/aptupdatechecker.deb aptupdatechecker-0.1.1.deb
+mv debian/aptupdatechecker.deb aptupdatechecker-$VERSION.deb
